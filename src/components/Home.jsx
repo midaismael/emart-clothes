8f976ae7fe69db4de3ab010c64da47f69df9a312
Ahmed Ismael <midaismael@gmail.com>
2022-08-01 20:43:03 +0200

    products

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 import background from '../assets/bg4.jpg'
+import Products from "./Products";
 
 const Home = () => {
   return (
     <div className="hero">
-      <div class="card text-bg-dark border-0">
-        <img src={background} class="card-img" alt="Background" height="550px"/>
-        <div class="card-img-overlay d-flex flex-column justify-content-center">
+      <div className="card text-bg-dark border-0">
+        <img src={background} className="card-img" alt="Background" height="550px"/>
+        <div className="card-img-overlay d-flex flex-column justify-content-center">
             <div className="contanier">
-            <h5 class="card-title display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
-          <p class="card-text fs-2 lead ">
+            <h5 className="card-title display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
+          <p className="card-text fs-2 lead ">
             CHECK OUT ALL THE TRENDS
           </p>
             </div>
         </div>
       </div>
+      <Products/>
     </div>
   );
 };
