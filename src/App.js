a405dbae34f05f17eb9d9ded8b669bbe1bd80582
Ahmed Ismael <midaismael@gmail.com>
2022-08-03 20:38:56 +0200

    ...cart

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home';
 import {Routes, Route} from 'react-router-dom';
 import Products from './components/Products';
 import Product from './components/Product';
+import Cart from './components/Cart';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Route exact path="/" element={<Home/>} />
       <Route exact path="/products" element={<Products/>} />
       <Route exact path="/products/:id" element={<Product/>} />
+      <Route exact path="/cart" element={<Cart/>} />
     </Routes>
     </>
   );
