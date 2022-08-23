f12f43bbb56783142a837195bb7ae6a29e2fb0bb
Ahmed Ismael <midaismael@gmail.com>
2022-08-23 22:13:47 +0200

    Remove homepage

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ function App() {
     <>
     <Navbar />
     <Routes>
-      <Route exact path="/" element={<Home/>} />
       <Route exact path="/products" element={<Products/>} />
       <Route exact path="/products/:id" element={<Product/>} />
       <Route exact path="/cart" element={<Cart/>} />
+      <Route exact path="/" element={<Home />} />
     </Routes>
     </>
   );
